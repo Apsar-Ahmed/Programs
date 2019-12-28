a6261cb59da98e6222b456e283a3b7ccf4b88911
Apsar Ahmad <apsar.sheik143@gmail.com>
2019-12-28 20:27:19 +0530

    Practice Programs

---
+++ Student/src/Apsar.java
@@ -0,0 +1,15 @@
+
+public class Apsar {
+	int salary=10000;
+	public static void main(String[] args) {
+		Apsar i=new Apsar();
+	
+		System.out.println(i.salary);
+		
+		
+	}
+	
+	
+	
+}
+
---
+++ Student/src/Student.java
@@ -0,0 +1,25 @@
+
+public class Student {
+	int rollnum;
+	public static void main(String[] args) {
+		Student s=new Student();
+		s.rollnum=10;
+		System.out.println(s.rollnum);
+		Student k = new Student();
+		k.rollnum = 20;
+		System.out.println(k.rollnum);
+		
+		Student i=new Student();
+		i.rollnum=40;
+		System.out.println(i.rollnum);
+		
+		
+		
+		
+		
+	}
+	{
+		
+	}
+
+}
